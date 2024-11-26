209a5ea23477c9d7a951ebb2c87d9d24912339d5
ramezhany1919 <ramezhany1919@gmail.com>
2024-11-26 23:02:58 +0200

    MileStone2

--- PIR_Sensor.h
+++ PIR_Sensor.h
@@ -3,10 +3,10 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-// Define the IR sensor pin
+
 #define IR_SENSOR_PIN 4
 
-// Function prototypes
+
 void ir_sensor_init(void);
 bool ir_sensor_detect(void);
 
